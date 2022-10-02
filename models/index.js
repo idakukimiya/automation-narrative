1c31b3cf225621f3d3eae562ecef073da1d891c4
idakukimiya <kukimiya.ida@gmail.com>
2022-10-01 21:57:55 -0400

    rename models files

--- models/index.js
+++ models/index.js
@@ -1,13 +1,39 @@
 const User = require('./User');
-const Gallery = require('./Gallery');
-const Painting = require('./Painting');
+const Post = require('./Post');
+// const Comment = require('./Comment');
 
-Gallery.hasMany(Painting, {
-  foreignKey: 'gallery_id',
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
 });
 
-Painting.belongsTo(Gallery, {
-  foreignKey: 'gallery_id',
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
 });
 
-module.exports = { User, Gallery, Painting };
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+})
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+})
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+})
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+})
+
+Post.belongsTo(Comment, {
+  foreignKey: 'comment_id'
+})
+
+Comment.hasOne(Post, {
+  foreignKey: 'comment_id'
+})
+
+module.exports = { User, Post };
