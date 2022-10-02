97b4e42487ac16c66d271b38462ae2608fe34f7a
idakukimiya <kukimiya.ida@gmail.com>
2022-10-01 23:04:22 -0400

    making it work

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
 const Post = require('./Post');
-// const Comment = require('./Comment');
+const Comment = require('./Comment');
 
 Post.belongsTo(User, {
   foreignKey: 'user_id'
@@ -36,4 +36,4 @@ Comment.hasOne(Post, {
   foreignKey: 'comment_id'
 })
 
-module.exports = { User, Post };
+module.exports = { User, Post, Comment };
