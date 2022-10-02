27804473a564d6c221626415cd024bfd411db004
idakukimiya <kukimiya.ida@gmail.com>
2022-10-01 20:54:07 -0400

    working on seed files

--- seeds/postData.js
+++ seeds/postData.js
@@ -1,14 +1,14 @@
 [
   {
-    "title": "Teslas Suck!",
-    "content": "I was driving around last night in my Tesla and the battery just died on me! Took it to the shop this morning but they told me I had to replace the whole battery! Had a buddy this happened to a while back, too. DON'T TRUST TESLA!"
+    "title": "Teslas!",
+    "content": "Is there another option?"
   },
   {
-    "title": "Tech Quiz",
-    "content": "Just found a way to test people on their technical skills in car machinery over the internet! Looking for people who are interested in trying it out, so hit me up if you're looking for a way to test your skills!"
+    "title": "What's new?",
+    "content": "Is python necessary to full stack develop?"
   },
   {
-    "title": "Solar-Powered A/C",
-    "content": "Started a project where I could possibly create a solar powered air conditioner, as air conditioners are always facing out the window. Follow this blog if you want to keep up with my journey!"
+    "title": "C# vs C++",
+    "content": "C++ is known as intermediate-level language that adds object-oriented features to tits base c, whereas C# is high-level language!"
   }
 ]
